b85bfc251eba2472f24f196ff8d23b3bc93b0413
David Williams <dpw341@gmail.com>
2014-02-04 14:35:07 +0000

    Add a minimum version of lein (since resource paths are weird in uberjars in lein 2.3.2 and below

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :min-lein-version "2.3.4"
+
   :main hermit.core
 
   :dependencies [[org.clojure/clojure "1.5.1"]
