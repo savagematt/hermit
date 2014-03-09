f89d6967b5ed9aa8cb257b0b2d6b07632c370ebe
Matt Savage <matthew.savage@gmail.com>
2014-03-09 16:08:32 +0000

    Bumping version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject savagematt/hermit "0.5"
+(defproject savagematt/hermit "0.6-SNAPSHOT"
   :description "Run command line scripts bundled within your clojure project "
 
   :url "https://github.com/savagematt/hermit"
