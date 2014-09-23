95d98c19cb4c6d516b3804a472d09099add51f11
David Williams <dpw341@gmail.com>
2014-09-23 07:20:51 +0100

    Add S3 wagon and lein-set-version plugins for build

--- project.clj
+++ project.clj
@@ -11,5 +11,8 @@
                  [me.raynes/fs "1.4.5"]
                  [me.raynes/conch "0.8.0"]]
 
+  :plugins [[s3-wagon-private "1.1.2"]
+            [lein-set-version "0.3.0"]]
+
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.0"]]}})
