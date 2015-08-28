33a60d9ae7cf49d60bcf5319daa9f9ccd4536fd0
David Williams <dpw341@gmail.com>
2015-08-28 13:54:19 +0100

    0.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject savagematt/hermit "0.9-SNAPSHOT"
+(defproject savagematt/hermit "0.9"
   :description "Run command line scripts bundled within your clojure project "
 
   :url "https://github.com/savagematt/hermit"
